2ebdf7923914d5f5f8ae619748649adf655ec7c6
francescoforte831 <francesco.forte831@gmail.com>
2026-04-17 20:02:20 -0400

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int front = 0;
+        int back = s.length() - 1;
+        while (back > front) {
+            if (!Character.isLetterOrDigit(s.charAt(front))) {
+                front++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(s.charAt(back))) {
+                back--;
+                continue;
+            }
+            if (s.charAt(front) != s.charAt(back)) {
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
+}
